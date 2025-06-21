a52693f1e37a77580da1ea2d7da729e68b48f768
alexbernal89 <142032828+alexbernal89@users.noreply.github.com>
2025-06-21 13:34:06 -0500

    Se ajusta la configuracioón de la BD

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLAlchemy database URl en render
-#SQLALCHEMY_DATABASE_URL = 'postgresql://database_c7cy_user:sAdHev0baTEDmOa0gVGmRKLb8jqBeSUh@dpg-d1bet40dl3ps73eknjvg-a/database_c7cy'
+SQLALCHEMY_DATABASE_URL = 'postgresql://database_c7cy_user:sAdHev0baTEDmOa0gVGmRKLb8jqBeSUh@dpg-d1bet40dl3ps73eknjvg-a/database_c7cy'
 #local
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres!@localhost:5432/TodoApplicationDatabase'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres!@localhost:5432/TodoApplicationDatabase'
 
 # MySQL database URL (uncomment if using MySQL)
 #SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:AlexBM1@127.0.0.1:3306/todoapplicationdatabase'
